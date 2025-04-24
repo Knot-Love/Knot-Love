31a655ff2b7c4bbefb92668ed54e8761353bc44c
Knot-Love <knotandlove156@gmail.com>
2025-04-24 20:52:36 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,88 +1,99 @@
-import { Outlet, useLocation } from 'react-router-dom'
-import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { Outlet, useLocation } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import toast, { Toaster } from 'react-hot-toast';
 import { useEffect } from 'react';
 import fetchUserDetails from './utils/fetchUserDetails';
 import { setUserDetails } from './store/userSlice';
-import { setAllCategory,setAllSubCategory,setLoadingCategory } from './store/productSlice';
+import { setAllCategory, setAllSubCategory, setLoadingCategory } from './store/productSlice';
 import { useDispatch } from 'react-redux';
 import Axios from './utils/Axios';
 import SummaryApi from './common/SummaryApi';
-import { handleAddItemCart } from './store/cartProduct'
-import GlobalProvider from './provider/GlobalProvider';
-import { FaCartShopping } from "react-icons/fa6";
+import GlobalProvider, { useGlobalContext } from './provider/GlobalProvider';
 import CartMobileLink from './components/CartMobile';
+import LoginModal from './components/LoginModal';
+import RegisterModal from './components/RegisterModal';
 
-function App() {
-  const dispatch = useDispatch()
-  const location = useLocation()
-  
+function AppContent() {
+  const dispatch = useDispatch();
+  const location = useLocation();
 
-  const fetchUser = async()=>{
-      const userData = await fetchUserDetails()
-      dispatch(setUserDetails(userData.data))
-  }
+  const {
+    showLoginModal,
+    setShowLoginModal,
+    showRegisterModal,
+    setShowRegisterModal
+  } = useGlobalContext();
 
-  const fetchCategory = async()=>{
+  const fetchUser = async () => {
+    const userData = await fetchUserDetails();
+    dispatch(setUserDetails(userData.data));
+  };
+
+  const fetchCategory = async () => {
     try {
-        dispatch(setLoadingCategory(true))
-        const response = await Axios({
-            ...SummaryApi.getCategory
-        })
-        const { data : responseData } = response
-
-        if(responseData.success){
-           dispatch(setAllCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name)))) 
-        }
-    } catch (error) {
-        
-    }finally{
-      dispatch(setLoadingCategory(false))
+      dispatch(setLoadingCategory(true));
+      const response = await Axios({ ...SummaryApi.getCategory });
+      if (response.data.success) {
+        dispatch(setAllCategory(response.data.data));
+      }
+    } catch (error) { } finally {
+      dispatch(setLoadingCategory(false));
     }
-  }
+  };
 
-  const fetchSubCategory = async()=>{
+  const fetchSubCategory = async () => {
     try {
-        const response = await Axios({
-            ...SummaryApi.getSubCategory
-        })
-        const { data : responseData } = response
+      const response = await Axios({ ...SummaryApi.getSubCategory });
+      if (response.data.success) {
+        dispatch(setAllSubCategory(response.data.data));
+      }
+    } catch (error) { }
+  };
 
-        if(responseData.success){
-           dispatch(setAllSubCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name)))) 
-        }
-    } catch (error) {
-        
-    }finally{
-    }
-  }
-
-  
-
-  useEffect(()=>{
-    fetchUser()
-    fetchCategory()
-    fetchSubCategory()
-    // fetchCartItem()
-  },[])
+  useEffect(() => {
+    fetchUser();
+    fetchCategory();
+    fetchSubCategory();
+  }, []);
 
   return (
-    <GlobalProvider> 
-      <Header/>
-      <main className='min-h-[78vh]'>
-          <Outlet/>
+    <>
+      <Header onLoginClick={() => setShowLoginModal(true)} />
+      <main className="min-h-[78vh]">
+        <Outlet />
       </main>
-      <Footer/>
-      <Toaster/>
-      {
-        location.pathname !== '/checkout' && (
-          <CartMobileLink/>
-        )
-      }
-    </GlobalProvider>
-  )
+      <Footer />
+      <Toaster />
+      {location.pathname !== '/checkout' && <CartMobileLink />}
+
+      {showLoginModal && (
+        <LoginModal
+          onClose={() => setShowLoginModal(false)}
+          onRegisterClick={() => {
+            setShowLoginModal(false);
+            setShowRegisterModal(true);
+          }}
+        />
+      )}
+
+      {showRegisterModal && (
+        <RegisterModal
+          onClose={() => setShowRegisterModal(false)}
+          setShowLoginModal={setShowLoginModal} // ✅ This was missing!
+        />
+      )}
+    </>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <GlobalProvider>
+      <AppContent />
+    </GlobalProvider>
+  );
+}
+
+export default App;
